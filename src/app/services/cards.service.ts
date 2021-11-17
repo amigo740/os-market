43dfcd17f9bdf42865d6fc3b523546ff2d071488
roman <amigo.85@list.ru>
2021-11-17 13:02:27 +0200

    fix adding new element

--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -5,22 +5,53 @@ import {CardModel, CardsInfo} from "../interfaces";
 @Injectable({
   providedIn: 'root'
 })
+
 export class CardsService {
 
   private cardsList: CardModel[] = cards;
 
   private isAddedWhenLoadedPage = false;
 
-  constructor() {}
+  private addCardListItem(obj: CardModel[]) {
+    obj.forEach( (item) => {
+      this.cardsList.push(item);
+    } );
+  }
+
+  private removeLocalStorageItem(id: string, localStorageName: string) {
+    let localStorageObj = JSON.parse( <string>localStorage.getItem(localStorageName) );
+    this.cardsList = this.cardsList.filter( item => item.id !== id );
+    if ( localStorageObj !== null ){
+      localStorageObj = localStorageObj.filter( (item: { id: string; }) => item.id !== id );
+      if (localStorageObj.length === 0){
+        localStorage.clear();
+      } else {
+        localStorage.setItem( localStorageName, JSON.stringify(localStorageObj) );
+      }
+    }
+  }
+
+  private static addLocalStorageItem(obj: CardModel, localStorageName: string) {
+    let localStorageObj = JSON.parse( <string>localStorage.getItem(localStorageName) );
+    if ( localStorageObj !== null ){
+      localStorageObj.push(obj);
+      localStorage.setItem( localStorageName, JSON.stringify(localStorageObj) );
+    } else {
+      localStorage.setItem( localStorageName, `[${JSON.stringify(obj)}]` );
+    }
+  }
 
   public getAllCards() {
     const localStorageObj = JSON.parse( <string>localStorage.getItem('app-market-products') );
+    const localStorageObj2 = JSON.parse( <string>localStorage.getItem('app-market-new-products') );
     if ( localStorageObj !== null ){
       if ( !this.isAddedWhenLoadedPage ){
-        for ( let i = 0; i < localStorageObj.length; i++ ){
-          this.cardsList.push(localStorageObj[i]);
-        }
+        this.addCardListItem(localStorageObj);
         this.isAddedWhenLoadedPage = true;
+        localStorage.removeItem('app-market-new-products');
+      } else if (localStorageObj2 !== null) {
+        this.addCardListItem(localStorageObj2);
+        localStorage.removeItem('app-market-new-products');
       }
     }
     return this.cardsList;
@@ -31,16 +62,7 @@ export class CardsService {
   }
 
   public removeCard( id: string ) {
-    let localStorageObj = JSON.parse( <string>localStorage.getItem('app-market-products') );
-    this.cardsList = this.cardsList.filter( item => item.id !== id );
-    if ( localStorageObj !== null ){
-      localStorageObj = localStorageObj.filter( (item: { id: string; }) => item.id !== id );
-      if (localStorageObj.length === 0){
-        localStorage.clear();
-      } else {
-        localStorage.setItem( 'app-market-products', JSON.stringify(localStorageObj) );
-      }
-    }
+    this.removeLocalStorageItem(id, 'app-market-products');
   }
 
   public getCardsInfo(): CardsInfo {
@@ -62,13 +84,9 @@ export class CardsService {
   }
 
   public addNewCard(obj: CardModel) {
-    let localStorageObj = JSON.parse( <string>localStorage.getItem('app-market-products') );
-    if ( localStorageObj !== null ){
-      localStorageObj.push(obj);
-      localStorage.setItem( 'app-market-products', JSON.stringify(localStorageObj) );
-    } else {
-      localStorage.setItem( 'app-market-products', `[${JSON.stringify(obj)}]` );
-    }
+    CardsService.addLocalStorageItem(obj, 'app-market-products');
+    CardsService.addLocalStorageItem(obj, 'app-market-new-products');
+    this.getAllCards();
   }
 
 }
